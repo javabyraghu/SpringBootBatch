a953014a65cbad20ee3345a449295a36e066071d
javabyraghu <javabyraghu@gmail.com>
2019-03-19 22:24:26 +0530

    Batch Updates

--- src/main/java/com/app/raghu/SpringBootBatchApplication.java
+++ src/main/java/com/app/raghu/SpringBootBatchApplication.java
@@ -1,11 +1,9 @@
 package com.app.raghu;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class SpringBootBatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/app/raghu/batch/config/BatchConfig.java
+++ src/main/java/com/app/raghu/batch/config/BatchConfig.java
@@ -3,6 +3,7 @@ package com.app.raghu.batch.config;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -16,6 +17,7 @@ import com.app.raghu.batch.reader.DataReader;
 import com.app.raghu.batch.writer.DataWriter;
 
 @Configuration
+@EnableBatchProcessing
 public class BatchConfig {
 	@Autowired
 	private JobBuilderFactory jobBuilderFactory;
@@ -28,8 +30,8 @@ public class BatchConfig {
 		return jobBuilderFactory.get("jobA")
 				.incrementer(new RunIdIncrementer())
 				.listener(listener())
-				.flow(stepA())
-				.end()
+				.start(stepA())
+				//.next(step)
 				.build();
 	}
 	
